846c7b14cd87b444112e17eb013af073a20b9e68
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-07-18 12:28:59 +0000

    first commit to Library Project

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 class Author(models.Model):
+    objects = None
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
+    objects = None
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
@@ -14,6 +16,7 @@ class Book(models.Model):
         return self.title
 
 class Library(models.Model):
+    objects = None
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
 
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,18 +1,23 @@
 import os
 import django
+from django.template.defaultfilters import title
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
 django.setup()
 
-from relationship_app.models import Author, Book, Library, Librarian
+from models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="John Doe")
+author = Author.objects.get(name="")
 books_by_author = Book.objects.filter(author=author)
 print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
 
+book = Book.objects.get(title="")
+list_all_books = book.books.all()
+
+
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name="library_name")
 books_in_library = library.books.all()
 print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
 
