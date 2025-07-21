e4c19c8eeb3ab7f13174e3b77b5977339ba6faac
zezor <kemma2993@gmail.com>
2025-07-20 22:24:48 -0800

    commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Create your views here.
 def all_books(request):
