93ee11ad00a57da26646f24e90a438f87834c749
zezor <kemma2993@gmail.com>
2025-07-27 17:18:48 -0800

    commit to advanced features

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,9 @@
+# LibraryProject/bookshelf/forms.py
+
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_date']
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+# LibraryProject/bookshelf/views.py
+
+from django.shortcuts import render, redirect
+from .forms import ExampleForm
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book-list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/add_book.html', {'form': form})
