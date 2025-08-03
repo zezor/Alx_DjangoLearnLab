7b4dfd45d6e7f14ea000406161fa301fe7ab8b0f
zezor <kemma2993@gmail.com>
2025-08-03 01:16:15 -0800

    commit to advanced features

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,12 @@
 # api/urls.py
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, BookList  # BookList is your earlier ListAPIView
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),  # includes all CRUD routes for BookViewSet
 ]
