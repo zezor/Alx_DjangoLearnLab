c766908dc6df75163879d45c371c63f275b9ea35
zezor <kemma2993@gmail.com>
2025-08-03 08:49:59 -0800

    commit to advanced features

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
+# api/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 from django.shortcuts import render
 
 # Create your views here.
