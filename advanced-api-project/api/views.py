441fd223f21dbbaa4e9bc318665a53eff8f4f54a
zezor <kemma2993@gmail.com>
2025-08-10 12:42:12 -0800

    commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters import rest_framework
+from rest_framework  import django_filters, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -33,6 +33,14 @@ class BookListView(generics.ListAPIView):
     # Ordering options
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']  # default ordering
+    
+class MyModelListAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['name', 'created_at']
+    ordering = ['name']  # default ordering
+
 
 
 class BookDetailView(generics.RetrieveAPIView):
