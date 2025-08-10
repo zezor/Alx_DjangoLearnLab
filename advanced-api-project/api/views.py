77096ca8d847517471e128dfafda93fdecb3e96e
zezor <kemma2993@gmail.com>
2025-08-10 12:50:22 -0800

    COMMIT

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework as filters
+from rest_framework import filters  # for SearchFilter & OrderingFilter
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -39,6 +40,7 @@ class MyModelListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [filters.OrderingFilter]
+    search_fields = [filters.SearchFilter]
     ordering_fields = ['name', 'created_at']
     ordering = ['name']  # default ordering
 
