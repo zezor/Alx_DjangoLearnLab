0cfa7d3d949688aad57dd1a302546969795deff1
zezor <kemma2993@gmail.com>
2025-08-10 12:13:33 -0800

    commit

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
+     path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
