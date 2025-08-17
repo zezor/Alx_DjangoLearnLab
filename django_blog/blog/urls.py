7bf6b5c63797df5ffe789ed505cc4ed74a2f70a6
zezor <kemma2993@gmail.com>
2025-08-17 15:01:56 -0800

    commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,4 +17,12 @@ urlpatterns = [
     path('posts/new/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+     
+     path('', PostListView.as_view(), name='post-list'),
+     
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
 ]
