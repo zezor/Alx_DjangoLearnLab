ae442aefecb04d5f865523b1c83d9005edd0197e
Emmanuel Nyamekye Nti <39943217+zezor@users.noreply.github.com>
2025-08-21 17:09:08 +0000

    updating registration view

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from .models import CustomUser
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,17 +12,24 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'first_name', 'last_name', 'bio', 'profile_picture')
         read_only_fields = ('id',)
 
+
+CustomUser = get_user_model()
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
+    token = serializers.CharField(read_only=True)  # ✅ include token field in response
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'password', 'first_name', 'last_name', 'date_of_birth', 'bio', 'location', 'profile_picture')
+        fields = (
+            'email', 'password', 'first_name', 'last_name',
+            'date_of_birth', 'bio', 'location', 'profile_picture', 'token'
+        )
 
     def create(self, validated_data):
-        user = CustomUser(
+        user = CustomUser.objects.create_user(   # ✅ best practice
             email=validated_data['email'],
+            password=validated_data['password'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
             date_of_birth=validated_data.get('date_of_birth'),
@@ -27,8 +37,9 @@ class RegisterSerializer(serializers.ModelSerializer):
             location=validated_data.get('location', ''),
             profile_picture=validated_data.get('profile_picture')
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        # ✅ create a token for this user
+        token = Token.objects.create(user=user)
+        user.token = token.key
         return user
 
 class LoginSerializer(serializers.Serializer):
